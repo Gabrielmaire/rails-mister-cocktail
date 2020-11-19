96484cc4e5500e24af6f97b46439c7ebf1a131bc
Gabriel Maire <gabrielmaire69@gmail.com>
2020-11-19 15:42:13 +0100

    cocktails / doses

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,35 @@
+class CocktailsController < ApplicationController
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    @cocktail.save!
+    redirect_to cocktails_path
+  end
+
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(cocktail_params)
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name, :ingredient_id)
+  end
+end
